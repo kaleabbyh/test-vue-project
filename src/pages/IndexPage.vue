2220742c1bd4ff01e1b4f7d6506ee4c7af931899
kaleabbyh <kaleabbyh@gmail.com>
2024-04-02 11:37:20 +0300

    add:cardsComponent.vue added && index.vue updated

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,205 +1,48 @@
 <template>
+  <div></div>
   <div class="q-pa-md">
-    <div class="q-col-gutter-md row items-start q-mb-lg q-py-lg">
-      <div class="col-12 col-md-6 col-lg-3">
-        <q-card flat style="border-radius: 8px">
-          <q-img src="~/assets/parallax1.jpg" />
-        </q-card>
-        <div class="flex q-pt-sm">
-          <q-avatar class="bg-white text-white" outlined>
-            <q-img src="~/assets/avatar.png"
-          /></q-avatar>
-          <div class="flex column q-pl-sm">
-            <span class="text-weight-bold">Title</span>
-            <span class="text-weight-normal">subtitle</span>
-            <span class="text-weight-thin">description</span>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-12 col-md-6 col-lg-3">
-        <q-card flat style="border-radius: 8px">
-          <q-img src="~/assets/parallax1.jpg" />
-        </q-card>
-        <div class="flex q-pt-sm">
-          <q-avatar class="bg-white text-white" outlined>
-            <q-img src="~/assets/avatar.png"
-          /></q-avatar>
-          <div class="flex column q-pl-sm">
-            <span class="text-weight-bold">Title</span>
-            <span class="text-weight-normal">subtitle</span>
-            <span class="text-weight-thin">description</span>
-          </div>
-        </div>
-        <!-- image two -->
-      </div>
-
-      <div class="col-12 col-md-6 col-lg-3">
-        <q-card flat style="border-radius: 8px">
-          <q-img src="~/assets/parallax1.jpg" />
-        </q-card>
-        <div class="flex q-pt-sm">
-          <q-avatar class="bg-white text-white" outlined>
-            <q-img src="~/assets/avatar.png"
-          /></q-avatar>
-          <div class="flex column q-pl-sm">
-            <span class="text-weight-bold">Title</span>
-            <span class="text-weight-normal">subtitle</span>
-            <span class="text-weight-thin">description</span>
-          </div>
-        </div>
-      </div>
-      <div class="col-12 col-md-6 col-lg-3">
-        <q-card flat style="border-radius: 8px">
-          <q-img src="~/assets/parallax1.jpg" />
-        </q-card>
-        <div class="flex q-pt-sm">
-          <q-avatar class="bg-white text-white" outlined>
-            <q-img src="~/assets/avatar.png"
-          /></q-avatar>
-          <div class="flex column q-pl-sm">
-            <span class="text-weight-bold">Title</span>
-            <span class="text-weight-normal">subtitle</span>
-            <span class="text-weight-thin">description</span>
-          </div>
-        </div>
-      </div>
+    <div class="q-col-gutter-md row items-start q-my-lg q-py-lg">
+      <CardsComponent v-for="index in 4" :key="index" v-bind="cards[0]" />
     </div>
     <div class="q-col-gutter-md row items-start q-my-lg q-py-lg">
-      <div class="col-12 col-md-6 col-lg-3">
-        <q-card flat style="border-radius: 8px">
-          <q-img src="~/assets/mountains.jpg" />
-        </q-card>
-        <div class="flex q-pt-sm">
-          <q-avatar class="bg-white text-white" outlined>
-            <q-img src="~/assets/avatar.png"
-          /></q-avatar>
-          <div class="flex column q-pl-sm">
-            <span class="text-weight-bold">Title</span>
-            <span class="text-weight-normal">subtitle</span>
-            <span class="text-weight-thin">description</span>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-12 col-md-6 col-lg-3">
-        <q-card flat style="border-radius: 8px">
-          <q-img src="~/assets/mountains.jpg" />
-        </q-card>
-        <div class="flex q-pt-sm">
-          <q-avatar class="bg-white text-white" outlined>
-            <q-img src="~/assets/avatar.png"
-          /></q-avatar>
-          <div class="flex column q-pl-sm">
-            <span class="text-weight-bold">Title</span>
-            <span class="text-weight-normal">subtitle</span>
-            <span class="text-weight-thin">description</span>
-          </div>
-        </div>
-        <!-- image two -->
-      </div>
-
-      <div class="col-12 col-md-6 col-lg-3">
-        <q-card flat style="border-radius: 8px">
-          <q-img src="~/assets/mountains.jpg" />
-        </q-card>
-        <div class="flex q-pt-sm">
-          <q-avatar class="bg-white text-white" outlined>
-            <q-img src="~/assets/avatar.png"
-          /></q-avatar>
-          <div class="flex column q-pl-sm">
-            <span class="text-weight-bold">Title</span>
-            <span class="text-weight-normal">subtitle</span>
-            <span class="text-weight-thin">description</span>
-          </div>
-        </div>
-      </div>
-      <div class="col-12 col-md-6 col-lg-3">
-        <q-card flat style="border-radius: 8px">
-          <q-img src="~/assets/mountains.jpg" />
-        </q-card>
-        <div class="flex q-pt-sm">
-          <q-avatar class="bg-white text-white" outlined>
-            <q-img src="~/assets/avatar.png"
-          /></q-avatar>
-          <div class="flex column q-pl-sm">
-            <span class="text-weight-bold">Title</span>
-            <span class="text-weight-normal">subtitle</span>
-            <span class="text-weight-thin">description</span>
-          </div>
-        </div>
-      </div>
+      <CardsComponent v-for="index in 4" :key="index" v-bind="cards[1]" />
     </div>
     <div class="q-col-gutter-md row items-start q-my-lg q-py-lg">
-      <div class="col-12 col-md-6 col-lg-3">
-        <q-card flat style="border-radius: 8px">
-          <q-img src="~/assets/parallax1.jpg" />
-        </q-card>
-        <div class="flex q-pt-sm">
-          <q-avatar class="bg-white text-white" outlined>
-            <q-img src="~/assets/avatar.png"
-          /></q-avatar>
-          <div class="flex column q-pl-sm">
-            <span class="text-weight-bold">Title</span>
-            <span class="text-weight-normal">subtitle</span>
-            <span class="text-weight-thin">description</span>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-12 col-md-6 col-lg-3">
-        <q-card flat style="border-radius: 8px">
-          <q-img src="~/assets/parallax1.jpg" />
-        </q-card>
-        <div class="flex q-pt-sm">
-          <q-avatar class="bg-white text-white" outlined>
-            <q-img src="~/assets/avatar.png"
-          /></q-avatar>
-          <div class="flex column q-pl-sm">
-            <span class="text-weight-bold">Title</span>
-            <span class="text-weight-normal">subtitle</span>
-            <span class="text-weight-thin">description</span>
-          </div>
-        </div>
-        <!-- image two -->
-      </div>
-
-      <div class="col-12 col-md-6 col-lg-3">
-        <q-card flat style="border-radius: 8px">
-          <q-img src="~/assets/parallax1.jpg" />
-        </q-card>
-        <div class="flex q-pt-sm">
-          <q-avatar class="bg-white text-white" outlined>
-            <q-img src="~/assets/avatar.png"
-          /></q-avatar>
-          <div class="flex column q-pl-sm">
-            <span class="text-weight-bold">Title</span>
-            <span class="text-weight-normal">subtitle</span>
-            <span class="text-weight-thin">description</span>
-          </div>
-        </div>
-      </div>
-      <div class="col-12 col-md-6 col-lg-3">
-        <q-card flat style="border-radius: 8px">
-          <q-img src="~/assets/parallax1.jpg" />
-        </q-card>
-        <div class="flex q-pt-sm">
-          <q-avatar class="bg-white text-white" outlined>
-            <q-img src="~/assets/avatar.png"
-          /></q-avatar>
-          <div class="flex column q-pl-sm">
-            <span class="text-weight-bold">Title</span>
-            <span class="text-weight-normal">subtitle</span>
-            <span class="text-weight-thin">description</span>
-          </div>
-        </div>
-      </div>
+      <CardsComponent v-for="index in 4" :key="index" v-bind="cards[2]" />
     </div>
   </div>
 </template>
 
 <script setup>
+import CardsComponent from "components/CardsComponent.vue";
+import parallaxImage from "../assets/parallax1.jpg";
+import mountainsImage from "../assets/mountains.jpg";
+
+import avatarImage from "../assets/avatar.png";
 defineOptions({
   name: "IndexPage",
 });
+const cards = [
+  {
+    image: parallaxImage,
+    avatar: avatarImage,
+    title: "Title",
+    subtitle: "Subtitle",
+    desc: "Description",
+  },
+  {
+    image: mountainsImage,
+    avatar: avatarImage,
+    title: "Title",
+    subtitle: "Subtitle",
+    desc: "Description",
+  },
+  {
+    image: parallaxImage,
+    avatar: avatarImage,
+    title: "Title",
+    subtitle: "Subtitle",
+    desc: "Description",
+  },
+];
 </script>
